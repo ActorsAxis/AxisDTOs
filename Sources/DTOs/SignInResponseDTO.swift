edd0184213b01401b4a68ae890469dd9a2cd4d17
William J. C. Nesbitt <wjcn@mac.com>
2024-10-03 16:41:02 -0700

    Adds initial sign-in support. Adds Swift 6 support.

---
+++ Sources/DTOs/SignInResponseDTO.swift
@@ -0,0 +1,16 @@
+//
+//  SignInResponseDTO.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 10/3/24.
+//
+
+public struct SignInResponseDTO: Codable, Sendable {
+	public let token: String
+
+	public init(
+		token: String
+	) {
+		self.token = token
+	}
+}
