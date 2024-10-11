726c44ac7180433c6805b71d07a9963b00efa4bb
William J. C. Nesbitt <wjcn@mac.com>
2024-10-10 17:42:01 -0700

    Added the token property to the SignInWithIdentityTokenResponseDTO.

--- Sources/DTOs/SignInDTOs.swift
+++ Sources/DTOs/SignInDTOs.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 public struct SignInWithIdentityTokenResponseDTO: Codable, Sendable {
+	public let token:   String
 	public let email:   String?
 	public let picture: String?
 
 	public init(
+		token:   String,
 		email:   String? = nil,
 		picture: String? = nil
 
 	) {
+		self.token   = token
 		self.email   = email
 		self.picture = picture
 	}
