9d5843493c4f4bdfa1a2de51dd750cbbd547436e
William J. C. Nesbitt <wjcn@mac.com>
2024-10-13 10:32:44 -0700

    Added optional user property to SignInWithIdentityTokenResponseDTO.

--- Sources/DTOs/SignInDTOs.swift
+++ Sources/DTOs/SignInDTOs.swift
@@ -11,15 +11,18 @@ public struct SignInWithIdentityTokenResponseDTO: Codable, Sendable {
 	public let token:   String
 	public let email:   String?
 	public let picture: String?
+	public let user:    UserResponseDTO?
 
 	public init(
 		token:   String,
-		email:   String? = nil,
-		picture: String? = nil
+		email:   String?          = nil,
+		picture: String?          = nil,
+		user:    UserResponseDTO? = nil
 
 	) {
 		self.token   = token
 		self.email   = email
 		self.picture = picture
+		self.user    = user
 	}
 }
