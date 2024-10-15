778c2cfe97ed827ec7f9aee3ccb03cad8e035829
William J. C. Nesbitt <wjcn@mac.com>
2024-10-14 18:39:08 -0700

    Adds givenName, familyName and name to the SignInWithIdentityTokenResponseDTO.

--- Sources/DTOs/SignInDTOs.swift
+++ Sources/DTOs/SignInDTOs.swift
@@ -8,21 +8,30 @@
 import Foundation
 
 public struct SignInWithIdentityTokenResponseDTO: Codable, Sendable {
-	public let token:   String
-	public let email:   String?
-	public let picture: String?
-	public let user:    UserResponseDTO?
+	public let axisToken:  String
+	public let email:      String?
+	public let givenName:  String?
+	public let familyName: String?
+	public let name:       String?
+	public let picture:    String?
+	public let user:       UserResponseDTO?
 
 	public init(
-		token:   String,
-		email:   String?          = nil,
-		picture: String?          = nil,
-		user:    UserResponseDTO? = nil
+		axisToken:  String,
+		email:      String?          = nil,
+		givenName:  String?          = nil,
+		familyName: String?          = nil,
+		name:       String?          = nil,
+		picture:    String?          = nil,
+		user:       UserResponseDTO? = nil
 
 	) {
-		self.token   = token
-		self.email   = email
-		self.picture = picture
-		self.user    = user
+		self.axisToken  = axisToken
+		self.email      = email
+		self.givenName  = givenName
+		self.familyName = familyName
+		self.name       = name
+		self.picture    = picture
+		self.user       = user
 	}
 }
