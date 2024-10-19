d0f1e09a20fe8d6986af2affd8cbbd49ec99c275
William J. C. Nesbitt <wjcn@mac.com>
2024-10-18 20:55:45 -0700

    Changes how account types, user levels, user types and work types are handled.

--- Sources/DTOs/Users/UserDTOs.swift
+++ Sources/DTOs/Users/UserDTOs.swift
@@ -10,10 +10,10 @@ import Foundation
 public struct UserRequest: Codable, Sendable {
 	public var id: UUID?
 
-	public var accountType: UUID?
-	public var userLevel:   UUID?
-	public var userType:    UUID?
-	public var workType:    UUID?
+	public var accountTypeID: UUID?
+	public var userLevelID:   UUID?
+	public var userTypeID:    UUID?
+	public var workTypeID:    UUID?
 
 	public var subject:    String?
 	public var email:      String?
@@ -24,19 +24,14 @@ public struct UserRequest: Codable, Sendable {
 	public var picture:    String?
 	public var biography:  String?
 	public var resume:     String?
-#if false
-	public var currentlyFilming: [CurrentlyFilmingRequest]
-	public var headshot:                  HeadshotRequest?
-	public var media:            [          MediumRequest]
-	public var works:            [            WorkRequest]
-#endif
+
 	public init(
 		id: UUID? = nil,
 
-		accountType: UUID? = nil,
-		userLevel:   UUID? = nil,
-		userType:    UUID? = nil,
-		workType:    UUID? = nil,
+		accountTypeID: UUID? = nil,
+		userLevelID:   UUID? = nil,
+		userTypeID:    UUID? = nil,
+		workTypeID:    UUID? = nil,
 
 		subject:    String? = nil,
 		email:      String? = nil,
@@ -47,20 +42,13 @@ public struct UserRequest: Codable, Sendable {
 		picture:    String? = nil,
 		biography:  String? = nil,
 		resume:     String? = nil
-/*
-		currentlyFilming: [CurrentlyFilmingRequest] = [],
-		headshot:                  HeadshotRequest? = nil,
-		media:            [          MediumRequest] = [],
-		works:            [            WorkRequest] = []
-		#endif
-*/
 	) {
 		self.id = id
 
-		self.accountType = accountType
-		self.userLevel   = userLevel
-		self.userType    = userType
-		self.workType    = workType
+		self.accountTypeID = accountTypeID
+		self.userLevelID   = userLevelID
+		self.userTypeID    = userTypeID
+		self.workTypeID    = workTypeID
 
 		self.subject    = subject
 		self.email      = email
@@ -71,38 +59,26 @@ public struct UserRequest: Codable, Sendable {
 		self.picture    = picture
 		self.biography  = biography
 		self.resume     = resume
-#if false
-		self.currentlyFilming = currentlyFilming
-		self.headshot         = headshot
-		self.media            = media
-		self.works            = works
-#endif
 	}
 
 	public init(from response: UserResponse) {
 		self.init(
-			id:               response.id,
+			id: response.id,
 
-			accountType:      response.accountType,
-			userLevel:        response.userLevel,
-			userType:         response.userType,
-			workType:         response.workType,
+			accountTypeID: response.accountTypeID,
+			userLevelID:   response.userLevelID,
+			userTypeID:    response.userTypeID,
+			workTypeID:    response.workTypeID,
 
-			subject:          response.subject,
-			email:            response.email,
-			password:         response.password,
-			givenName:        response.givenName,
-			familyName:       response.familyName,
-			name:             response.name,
-			picture:          response.picture,
-			biography:        response.biography,
-			resume:           response.resume
-/*
-			currentlyFilming: response.currentlyFilming.map(CurrentlyFilmingRequest.init),
-			headshot:         response.headshot    .flatMap(HeadshotRequest.init),
-			media:            response.media           .map(MediumRequest.init),
-			works:            response.works           .map(WorkRequest.init)
-*/
+			subject:    response.subject,
+			email:      response.email,
+			password:   response.password,
+			givenName:  response.givenName,
+			familyName: response.familyName,
+			name:       response.name,
+			picture:    response.picture,
+			biography:  response.biography,
+			resume:     response.resume
 		)
 	}
 }
@@ -113,10 +89,10 @@ public struct UserResponse: Codable,  Sendable,
 							Hashable, Identifiable {
 	public var id: UUID
 
-	public var accountType: UUID?
-	public var userLevel:   UUID?
-	public var userType:    UUID?
-	public var workType:    UUID?
+	public var accountTypeID: UUID?
+	public var userLevelID:   UUID?
+	public var userTypeID:    UUID?
+	public var workTypeID:    UUID?
 
 	public var subject:    String?
 	public var email:      String?
@@ -127,19 +103,14 @@ public struct UserResponse: Codable,  Sendable,
 	public var picture:    String?
 	public var biography:  String?
 	public var resume:     String?
-#if false
-	public var currentlyFilming: [CurrentlyFilmingResponse]
-	public var headshot:                  HeadshotResponse?
-	public var media:            [          MediumResponse]
-	public var works:            [            WorkResponse]
-#endif
+
 	public init(
 		id: UUID,
 
-		accountType: UUID? = nil,
-		userLevel:   UUID? = nil,
-		userType:    UUID? = nil,
-		workType:    UUID? = nil,
+		accountTypeID: UUID? = nil,
+		userLevelID:   UUID? = nil,
+		userTypeID:    UUID? = nil,
+		workTypeID:    UUID? = nil,
 
 		subject:    String? = nil,
 		email:      String? = nil,
@@ -150,19 +121,13 @@ public struct UserResponse: Codable,  Sendable,
 		picture:    String? = nil,
 		biography:  String? = nil,
 		resume:     String? = nil
-/*
-		currentlyFilming: [CurrentlyFilmingResponse] = [],
-		headshot:                  HeadshotResponse? = nil,
-		media:            [          MediumResponse] = [],
-		works:            [            WorkResponse] = []
-*/
 	) {
 		self.id = id
 
-		self.accountType = accountType
-		self.userLevel   = userLevel
-		self.userType    = userType
-		self.workType    = workType
+		self.accountTypeID = accountTypeID
+		self.userLevelID   = userLevelID
+		self.userTypeID    = userTypeID
+		self.workTypeID    = workTypeID
 
 		self.subject    = subject
 		self.email      = email
@@ -173,33 +138,24 @@ public struct UserResponse: Codable,  Sendable,
 		self.picture    = picture
 		self.biography  = biography
 		self.resume     = resume
-#if false
-		self.currentlyFilming = currentlyFilming
-		self.headshot         = headshot
-		self.media            = media
-		self.works            = works
-#endif
 	}
 
 	public mutating func copyNonOptionals(from request: UserRequest) {
 		if let id = request.id { self.id = id }
 
-		if let accountType = request.accountType { self.accountType = accountType }
-		if let userLevel   = request.userLevel   { self.userLevel   = userLevel }
-		if let userType    = request.userType    { self.userType    = userType }
-		if let workType    = request.workType    { self.workType    = workType }
+		if let accountTypeID = request.accountTypeID { self.accountTypeID = accountTypeID }
+		if let userLevelID   = request.userLevelID   { self.userLevelID   = userLevelID   }
+		if let userTypeID    = request.userTypeID    { self.userTypeID    = userTypeID    }
+		if let workTypeID    = request.workTypeID    { self.workTypeID    = workTypeID    }
 
-		if let subject    = request.subject    { self.subject    = subject }
-		if let email      = request.email      { self.email      = email   }
-		if let password   = request.password   { self.password   = password }
-		if let givenName  = request.givenName  { self.givenName  = givenName }
+		if let subject    = request.subject    { self.subject    = subject    }
+		if let email      = request.email      { self.email      = email      }
+		if let password   = request.password   { self.password   = password   }
+		if let givenName  = request.givenName  { self.givenName  = givenName  }
 		if let familyName = request.familyName { self.familyName = familyName }
-		if let name       = request.name       { self.name       = name }
-		if let picture    = request.picture    { self.picture    = picture }
-		if let biography  = request.biography  { self.biography  = biography }
-		if let resume     = request.resume     { self.resume     = resume }
-#if false
-		// TODO: <#Comment#>
-#endif
+		if let name       = request.name       { self.name       = name       }
+		if let picture    = request.picture    { self.picture    = picture    }
+		if let biography  = request.biography  { self.biography  = biography  }
+		if let resume     = request.resume     { self.resume     = resume     }
 	}
 }
