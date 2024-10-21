d2f21e5553535675a189cc7a168569db2f896138
William J. C. Nesbitt <wjcn@mac.com>
2024-10-21 12:30:31 -0700

    Adds union status to headshot DTOs.

--- Sources/DTOs/Users/HeadshotDTOs.swift
+++ Sources/DTOs/Users/HeadshotDTOs.swift
@@ -16,6 +16,7 @@ public struct HeadshotRequest: Codable, Sendable {
 	public var phone:       String?
 	public var picture:     String?
 	public var socialMedia: String?
+	public var unionStatus: String?
 	public var website:     String?
 	public var weight:      String?
 
@@ -28,6 +29,7 @@ public struct HeadshotRequest: Codable, Sendable {
 		phone:       String? = nil,
 		picture:     String? = nil,
 		socialMedia: String? = nil,
+		unionStatus: String? = nil,
 		website:     String? = nil,
 		weight:      String? = nil
 	) {
@@ -39,6 +41,7 @@ public struct HeadshotRequest: Codable, Sendable {
 		self.phone       = phone
 		self.picture     = picture
 		self.socialMedia = socialMedia
+		self.unionStatus = unionStatus
 		self.website     = website
 		self.weight      = weight
 	}
@@ -53,6 +56,7 @@ public struct HeadshotRequest: Codable, Sendable {
 			phone:       response.phone,
 			picture:     response.picture,
 			socialMedia: response.socialMedia,
+			unionStatus: response.unionStatus,
 			website:     response.website,
 			weight:      response.weight
 		)
@@ -71,6 +75,7 @@ public struct HeadshotResponse: Codable,  Sendable,
 	public var phone:       String?
 	public var picture:     String?
 	public var socialMedia: String?
+	public var unionStatus: String?
 	public var website:     String?
 	public var weight:      String?
 
@@ -83,6 +88,7 @@ public struct HeadshotResponse: Codable,  Sendable,
 		phone:       String? = nil,
 		picture:     String? = nil,
 		socialMedia: String? = nil,
+		unionStatus: String? = nil,
 		website:     String? = nil,
 		weight:      String? = nil
 	) {
@@ -94,6 +100,7 @@ public struct HeadshotResponse: Codable,  Sendable,
 		self.phone       = phone
 		self.picture     = picture
 		self.socialMedia = socialMedia
+		self.unionStatus = unionStatus
 		self.website     = website
 		self.weight      = weight
 	}
@@ -107,6 +114,7 @@ public struct HeadshotResponse: Codable,  Sendable,
 		if let phone       = request.phone       { self.phone       = phone       }
 		if let picture     = request.picture     { self.picture     = picture     }
 		if let socialMedia = request.socialMedia { self.socialMedia = socialMedia }
+		if let unionStatus = request.unionStatus { self.unionStatus = unionStatus }
 		if let website     = request.website     { self.website     = website     }
 		if let weight      = request.weight      { self.weight      = weight      }
 	}
