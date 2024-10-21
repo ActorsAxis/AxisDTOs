b7aa536839682d6c34324b9b9d939a9617251257
William J. C. Nesbitt <wjcn@mac.com>
2024-10-21 13:11:31 -0700

    Renames HeadshotRequest to HeadshotDTO.

---
+++ Sources/DTOs/Users/HeadshotDTO.swift
@@ -0,0 +1,48 @@
+//
+//  HeadshotDTO.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 10/8/24.
+//
+
+import Foundation
+
+public struct HeadshotDTO: Codable, Sendable {
+	public var id:          UUID?
+	public var eyeColor:    String?
+	public var hairColor:   String?
+	public var height:      String?
+	public var location:    String?
+	public var phone:       String?
+	public var picture:     String?
+	public var socialMedia: String?
+	public var unionStatus: String?
+	public var website:     String?
+	public var weight:      String?
+
+	public init(
+		id:          UUID?   = nil,
+		eyeColor:    String? = nil,
+		hairColor:   String? = nil,
+		height:      String? = nil,
+		location:    String? = nil,
+		phone:       String? = nil,
+		picture:     String? = nil,
+		socialMedia: String? = nil,
+		unionStatus: String? = nil,
+		website:     String? = nil,
+		weight:      String? = nil
+	) {
+		self.id          = id
+		self.eyeColor    = eyeColor
+		self.hairColor   = hairColor
+		self.height      = height
+		self.location    = location
+		self.phone       = phone
+		self.picture     = picture
+		self.socialMedia = socialMedia
+		self.unionStatus = unionStatus
+		self.website     = website
+		self.weight      = weight
+	}
+}
