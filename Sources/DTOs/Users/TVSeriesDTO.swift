d7cc85a0f694ca474f867c72266e9739b10c4222
William J. C. Nesbitt <wjcn@mac.com>
2024-10-22 14:15:29 -0700

    Adds properties to TVSeriesDTO.

--- Sources/DTOs/Users/TVSeriesDTO.swift
+++ Sources/DTOs/Users/TVSeriesDTO.swift
@@ -11,18 +11,30 @@ public struct TVSeriesDTO: Codable,  Sendable,
 						   Hashable, Identifiable {
 	public let id:         UUID
 	public var name:       String?
-	public var number:     Int?
 	public var creditedAs: String?
+	public var season:     Int?
+	public var episode:    Int?
+	public var title:      String?
+	public var startYear:  Int?
+	public var endYear:    Int?
 
 	public init(
 		id:         UUID    = UUID(),
 		name:       String? = nil,
-		number:     Int?    = nil,
-		creditedAs: String? = nil
+		creditedAs: String? = nil,
+		season:     Int?    = nil,
+		episode:    Int?    = nil,
+		title:      String? = nil,
+		startYear:  Int?    = nil,
+		endYear:    Int?    = nil
 	) {
 		self.id         = id
 		self.name       = name
-		self.number     = number
 		self.creditedAs = creditedAs
+		self.season     = season
+		self.episode    = episode
+		self.title      = title
+		self.startYear  = startYear
+		self.endYear    = endYear
 	}
 }
