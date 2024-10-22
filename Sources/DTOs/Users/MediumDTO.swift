5000e916c86d7e433bff81046ccc901a6b4e6407
William J. C. Nesbitt <wjcn@mac.com>
2024-10-22 15:01:14 -0700

    Adds properties to MediumDTO.

--- Sources/DTOs/Users/MediumDTO.swift
+++ Sources/DTOs/Users/MediumDTO.swift
@@ -9,14 +9,26 @@ import Foundation
 
 public struct MediumDTO: Codable,  Sendable,
 						 Hashable, Identifiable {
-	public let id:     UUID
-	public var medium: String?
+	public let id:                     UUID
+	public var medium:                 String?
+	public var imageWidth:             Int?
+	public var imageHeight:            Int?
+	public var videoDurationInSeconds: Int?
+	public var videoThumbnail:         String?
 
 	public init(
-		id:     UUID    = UUID(),
-		medium: String? = nil
+		id:                     UUID    = UUID(),
+		medium:                 String? = nil,
+		imageWidth:             Int?    = nil,
+		imageHeight:            Int?    = nil,
+		videoDurationInSeconds: Int?    = nil,
+		videoThumbnail:         String? = nil
 	) {
-		self.id     = id
-		self.medium = medium
+		self.id                     = id
+		self.medium                 = medium
+		self.imageWidth             = imageWidth
+		self.imageHeight            = imageHeight
+		self.videoDurationInSeconds = videoDurationInSeconds
+		self.videoThumbnail         = videoThumbnail
 	}
 }
