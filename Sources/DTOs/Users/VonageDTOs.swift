3f29de779e5c967105f76c299764eb5761858ef8
William J. C. Nesbitt <wjcn@mac.com>
2024-11-01 16:15:30 -0700

    Modifies VonageDTOs.

--- Sources/DTOs/Users/VonageDTOs.swift
+++ Sources/DTOs/Users/VonageDTOs.swift
@@ -1,5 +1,5 @@
 //
-//  VonageDTO.swift
+//  VonageDTOs.swift
 //  AxisDTOs
 //
 //  Created by William J. C. Nesbitt on 11/1/24.
@@ -7,46 +7,108 @@
 
 import Foundation
 
-public struct VonageRequestDTO: Codable, Sendable {
-	public enum ChannelType: String {
-		case email
-		case silentAuthentication = "silent_auth"
-		case sms
-		case voice
-		case whatsApp             = "whatsapp"
+public enum VonageRequest {
+	public struct RequestDTO {
+		public struct Workflow: Codable, Sendable {
+			public enum Channel: String {
+				case email
+				case silentAuthentication = "silent_auth"
+				case sms
+				case voice
+				case whatsApp             = "whatsapp"
+			}
+
+			public let channel:         String
+			public let to:              String
+			public let from:            String?
+			public let entityID:        String?
+			public let contentID:       String?
+			public let applicationHash: String?
+
+			public init(
+				channel:         Channel,
+				to:              String,
+				from:            String? = nil,
+				entityID:        String? = nil,
+				contentID:       String? = nil,
+				applicationHash: String? = nil
+			) {
+				self.channel         = channel.rawValue
+				self.to              = to
+				self.from            = from
+				self.entityID        = entityID
+				self.contentID       = contentID
+				self.applicationHash = applicationHash
+			}
+
+			private enum CodingKeys: String, CodingKey {
+				case channel
+				case to
+				case from
+				case entityID        = "entity_id"
+				case contentID       = "content_id"
+				case applicationHash = "app_hash"
+			}
+		}
+
+		public let locale:          String?
+		public let channelTimeout:  Int?
+		public let clientReference: String?
+		public let codeLength:      Int?
+		public let code:            String?
+		public let brand:           String
+		public let templateID:      UUID?
+		public let workflow:       [Workflow]
+
+		public init(
+			locale:          String?,
+			channelTimeout:  Int?,
+			clientReference: String?,
+			codeLength:      Int?,
+			code:            String?,
+			brand:           String = .vonage.brand,
+			templateID:      UUID?,
+			workflow:       [Workflow]
+		) {
+			self.locale          = locale
+			self.channelTimeout  = channelTimeout
+			self.clientReference = clientReference
+			self.codeLength      = codeLength
+			self.code            = code
+			self.brand           = brand
+			self.templateID      = templateID
+			self.workflow        = workflow
+		}
+
+		private enum CodingKeys: String, CodingKey {
+			case locale
+			case channelTimeout  = "channel_timeout"
+			case clientReference = "client_ref"
+			case codeLength      = "code_length"
+			case code
+			case brand
+			case templateID      = "template_id"
+			case workflow
+		}
 	}
 
-	public let channel: String
-	public let from:    String?
-	public let to:      String
+	// MARK: -
 
-	public init(
-		channel: ChannelType,
-		from:    String? = nil,
-		to:      String
-	) {
-		self.channel = channel.rawValue
-		self.from    = from
-		self.to      = to
-	}
-}
+	public struct ResponseDTO: Codable, Sendable {
+		public let requestID: String
+		public let checkURL:  URL?
 
-// MARK: -
+		public init(
+			requestID: String,
+			checkURL:  URL? = nil
+		) {
+			self.requestID = requestID
+			self.checkURL  = checkURL
+		}
 
-public struct VonageResponseDTO: Codable, Sendable {
-	public let requestID: String
-	public let checkURL:  URL
-
-	public init(
-		requestID: String,
-		checkURL:  URL
-	) {
-		self.requestID = requestID
-		self.checkURL  = checkURL
-	}
-
-	private enum CodingKeys: String, CodingKey {
-		case requestID = "request_id"
-		case checkURL  = "check_url"
+		private enum CodingKeys: String, CodingKey {
+			case requestID = "request_id"
+			case checkURL  = "check_url"
+		}
 	}
 }
---
+++ Sources/Extensions/StringExtension.swift
@@ -0,0 +1,12 @@
+//
+//  StringExtension.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 11/1/24.
+//
+
+extension String {
+	public enum vonage {
+		public static let brand = "Axis OTP"
+	}
+}
