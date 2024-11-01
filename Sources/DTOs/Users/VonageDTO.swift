a7f5a2d725b720a1c73eb2a4ec1da424ceb10c5f
William J. C. Nesbitt <wjcn@mac.com>
2024-11-01 12:56:01 -0700

    Adds VonageDTO.

---
+++ Sources/DTOs/Users/VonageDTO.swift
@@ -0,0 +1,30 @@
+//
+//  VonageDTO.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 11/1/24.
+//
+
+public struct VonageDTO {
+	public enum ChannelType: String {
+		case email
+		case silentAuthentication = "silent_auth"
+		case sms
+		case voice
+		case whatsApp             = "whatsapp"
+	}
+
+	public let channel: String
+	public let from:    String?
+	public let to:      String
+
+	public init(
+		channel: ChannelType,
+		from:    String? = nil,
+		to:      String
+	) {
+		self.channel = channel.rawValue
+		self.from    = from
+		self.to      = to
+	}
+}
