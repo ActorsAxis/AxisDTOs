84f9f17c6fa3541c3a70931cbb229e89260a94bf
William J. C. Nesbitt <wjcn@mac.com>
2024-11-21 03:51:40 -0800

    Improves the uiniqueID algorithm.

--- Sources/Extensions/UUIDExtension.swift
+++ Sources/Extensions/UUIDExtension.swift
@@ -10,19 +10,14 @@ import zlib
 
 extension UUID {
 	public var uniqueID: UInt32 {
-		// Convert the UUID to Data (128 bits / 16 bytes).
-		let data = withUnsafeBytes(of: uuid) { Data($0) }
-
-		// Compute the CRC32 hash.
-		let crc = data.withUnsafeBytes { buffer -> UInt in
-			crc32(
-				0,
-				buffer.baseAddress!.assumingMemoryBound(to: UInt8.self),
-				UInt32(buffer.count)
+		withUnsafeBytes(of: uuid) { buffer in
+			UInt32(
+				truncatingIfNeeded: crc32(
+					0,
+					buffer.bindMemory(to: UInt8.self).baseAddress,
+					UInt32(buffer.count)
+				)
 			)
 		}
-
-		// Convert the UInt returned by crc32 to UInt32.
-		return UInt32(truncatingIfNeeded: crc)
 	}
 }
