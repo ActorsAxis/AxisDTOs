ead943b9646d894ce348bf7fbc823e7332e140f4
William J. C. Nesbitt <wjcn@mac.com>
2024-11-29 15:23:29 -0800

    Renames socialMediaTypeID to socialMediumTypeID.

--- Sources/DTOs/SocialMediumDTO.swift
+++ Sources/DTOs/SocialMediumDTO.swift
@@ -9,20 +9,20 @@ import Foundation
 
 public struct SocialMediumDTO: Codable,  Sendable,
 							   Hashable, Identifiable {
-	public let id:                UUID?
-	public var eventID:           UUID
-	public var socialMediaTypeID: UUID
-	public var path:              String
+	public let id:                 UUID?
+	public var eventID:            UUID
+	public var socialMediumTypeID: UUID
+	public var path:               String
 
 	public init(
-		id:                UUID? = nil,
-		eventID:           UUID  = UUID(),
-		socialMediaTypeID: UUID  = UUID(),
-		path:              String
+		id:                 UUID? = nil,
+		eventID:            UUID  = UUID(),
+		socialMediumTypeID: UUID  = UUID(),
+		path:               String
 	) {
-		self.id                = id
-		self.eventID           = eventID
-		self.socialMediaTypeID = socialMediaTypeID
-		self.path              = path
+		self.id                 = id
+		self.eventID            = eventID
+		self.socialMediumTypeID = socialMediumTypeID
+		self.path               = path
 	}
 }
