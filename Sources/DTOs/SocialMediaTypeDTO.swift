586413b6c2aa81afa80f5664bd81c4d2b5f8739c
William J. C. Nesbitt <wjcn@mac.com>
2024-11-29 00:07:30 -0800

    Adds SocialMediaTypeDTO.

---
+++ Sources/DTOs/SocialMediaTypeDTO.swift
@@ -0,0 +1,25 @@
+//
+//  SocialMediaTypeDTO.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 11/28/24.
+//
+
+import Foundation
+
+public struct SocialMediaTypeDTO: Codable,  Sendable,
+								  Hashable, Identifiable {
+	public let id:   UUID
+	public let name: String
+	public let logo: String?
+
+	public init(
+		id:   UUID,
+		name: String,
+		logo: String? = nil
+	) {
+		self.id   = id
+		self.name = name
+		self.logo = logo
+	}
+}
