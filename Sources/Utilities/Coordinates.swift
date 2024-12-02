f2e26f86e99f50d04cd41910c0817abe1bfd8c1b
William J. C. Nesbitt <wjcn@mac.com>
2024-12-02 10:27:35 -0800

    Removes CoreLocation dependency.

--- Sources/Utilities/Coordinates.swift
+++ Sources/Utilities/Coordinates.swift
@@ -5,21 +5,15 @@
 //  Created by William J. C. Nesbitt on 12/1/24.
 //
 
-import CoreLocation
-
 public struct Coordinates: Codable, Sendable {
 	public var latitude:  Double
 	public var longitude: Double
 
-	public init(latitude: Double, longitude: Double) {
+	public init(
+		latitude:  Double,
+		longitude: Double
+	) {
 		self.latitude  = latitude
 		self.longitude = longitude
-	}
-
-	public init(from coordinates: CLLocationCoordinate2D) {
-		self.init(
-			latitude:  coordinates.latitude,
-			longitude: coordinates.longitude
-		)
 	}
 }
