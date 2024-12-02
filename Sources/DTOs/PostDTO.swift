4f7e078b716624cd4678cd18fea2f115d9188a01
William J. C. Nesbitt <wjcn@mac.com>
2024-12-01 22:18:53 -0800

    Updates PostDTO.

--- Sources/DTOs/PostDTO.swift
+++ Sources/DTOs/PostDTO.swift
@@ -11,15 +11,27 @@ public struct PostDTO: Codable,  Sendable,
 					   Hashable, Identifiable {
 	public let id:          UUID
 	public var userID:      UUID
+	public var unionTypeID: UUID
 	public var coordinates: Coordinates?
+	public var name:        String
+	public var description: String?
+	public var url:         String?
 
 	public init(
 		id:          UUID,
-		userID:      UUID = UUID(),
-		coordinates: Coordinates? = nil
+		userID:      UUID         = UUID(),
+		unionTypeID: UUID         = UUID(),
+		coordinates: Coordinates? = nil,
+		name:        String,
+		description: String?      = nil,
+		url:         String?      = nil
 	) {
 		self.id          = id
 		self.userID      = userID
+		self.unionTypeID = unionTypeID
 		self.coordinates = coordinates
+		self.name        = name
+		self.description = description
+		self.url         = url
 	}
 }
