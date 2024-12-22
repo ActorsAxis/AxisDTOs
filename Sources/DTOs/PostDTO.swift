315b857dd826357ce191ed21c37747c51e64e807
William J. C. Nesbitt <wjcn@mac.com>
2024-12-21 21:23:34 -0800

    Adds color to PostTypeDTO. Removes color from PostDTO.

--- Sources/DTOs/PostDTO.swift
+++ Sources/DTOs/PostDTO.swift
@@ -15,7 +15,6 @@ public struct PostDTO: Codable,  Sendable,
 	public var userID:      UUID
 	public var user:        UserDTO?
 	public var coordinates: Coordinates?
-	public var color:       UInt32?
 	public var name:        String?
 	public var description: String?
 	public var url:         String?
@@ -32,7 +31,6 @@ public struct PostDTO: Codable,  Sendable,
 		userID:      UUID         = UUID(),
 		user:        UserDTO?     = nil,
 		coordinates: Coordinates? = nil,
-		color:       UInt32?      = nil,
 		name:        String?      = nil,
 		description: String?      = nil,
 		url:         String?      = nil,
@@ -48,7 +46,6 @@ public struct PostDTO: Codable,  Sendable,
 		self.userID      = userID
 		self.user        = user
 		self.coordinates = coordinates
-		self.color       = color
 		self.name        = name
 		self.description = description
 		self.url         = url
--- Sources/DTOs/PostTypeDTO.swift
+++ Sources/DTOs/PostTypeDTO.swift
@@ -11,17 +11,20 @@ public struct PostTypeDTO: Codable,  Sendable,
 						   Hashable, Identifiable {
 	public let id:      UUID
 	public var name:    String
+	public var color:   UInt32?
 	public let created: Date?
 	public let updated: Date?
 
 	public init(
 		id:      UUID,
 		name:    String,
-		created: Date? = nil,
-		updated: Date? = nil
+		color:   UInt32? = nil,
+		created: Date?   = nil,
+		updated: Date?   = nil
 	) {
 		self.id      = id
 		self.name    = name
+		self.color   = color
 		self.created = created
 		self.updated = updated
 	}
