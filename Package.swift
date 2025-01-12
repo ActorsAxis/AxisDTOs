9f08dd382daeb6886cb21eaa7923bbc81e8541fd
Tony <41577315+LilMoke@users.noreply.github.com>
2025-01-12 09:29:00 -0500

    Updated https://github.com/apple/swift-collections

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
