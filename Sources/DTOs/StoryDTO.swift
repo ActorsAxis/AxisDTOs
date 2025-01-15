cf1bb1fddd8d06608c51186f2335ee12cea84863
William J. C. Nesbitt <wjcn@mac.com>
2025-01-15 12:25:06 -0800

    Adds the UI property to StoryDTO.

--- Sources/DTOs/StoryDTO.swift
+++ Sources/DTOs/StoryDTO.swift
@@ -10,25 +10,38 @@ import Foundation
 public struct StoryDTO: Codable, Hashable, Sendable,
 						Identifiable {
 	public let id:      UUID
-	public let userID:  UUID?
-	public let user:    UserDTO?
-	public let media:  [MediumDTO]?
+	public var userID:  UUID?
+	public var user:    UserDTO?
+	public var media:  [MediumDTO]?
+	public var ui:      UserInterface?
 	public let created: Date?
 	public let updated: Date?
 
 	public init(
-		id:      UUID        = UUID(),
-		userID:  UUID?       = nil,
-		user:    UserDTO?    = nil,
-		media:  [MediumDTO]? = nil,
-		created: Date?       = nil,
-		updated: Date?       = nil
+		id:      UUID           = UUID(),
+		userID:  UUID?          = nil,
+		user:    UserDTO?       = nil,
+		media:  [MediumDTO]?    = nil,
+		ui:      UserInterface? = nil,
+		created: Date?          = nil,
+		updated: Date?          = nil
 	) {
 		self.id      = id
 		self.userID  = userID
 		self.user    = user
 		self.media   = media
+		self.ui      = ui
 		self.created = created
 		self.updated = updated
+	}
+
+	public struct UserInterface: Codable, Hashable, Sendable {
+		public var seen: Bool
+
+		public init(
+			seen: Bool = false
+		) {
+			self.seen = seen
+		}
 	}
 }
