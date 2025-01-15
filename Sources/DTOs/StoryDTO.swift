24207a9595ece3df2d0bb29fd11d3ff8f6804a78
William J. C. Nesbitt <wjcn@mac.com>
2025-01-15 10:17:49 -0800

    Adds media and user to StoryDTO.

--- Sources/DTOs/StoryDTO.swift
+++ Sources/DTOs/StoryDTO.swift
@@ -11,17 +11,23 @@ public struct StoryDTO: Codable, Hashable, Sendable,
 						Identifiable {
 	public let id:      UUID
 	public let userID:  UUID?
+	public let user:    UserDTO?
+	public let media:  [MediumDTO]?
 	public let created: Date?
 	public let updated: Date?
 
 	public init(
-		id:      UUID  = UUID(),
-		userID:  UUID? = nil,
-		created: Date? = nil,
-		updated: Date? = nil
+		id:      UUID        = UUID(),
+		userID:  UUID?       = nil,
+		user:    UserDTO?    = nil,
+		media:  [MediumDTO]? = nil,
+		created: Date?       = nil,
+		updated: Date?       = nil
 	) {
 		self.id      = id
 		self.userID  = userID
+		self.user    = user
+		self.media   = media
 		self.created = created
 		self.updated = updated
 	}
