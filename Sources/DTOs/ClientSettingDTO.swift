2301cb343ca9c601b08e4f9f81543d0e527972d4
William J. C. Nesbitt <wjcn@mac.com>
2025-02-20 13:20:08 -0800

    Adds support for client settings.

---
+++ Sources/DTOs/ClientSettingDTO.swift
@@ -0,0 +1,52 @@
+//
+//  ClientSettingDTO.swift
+//  AxisDTOs
+//
+//  Created by William J. C. Nesbitt on 2/17/25.
+//
+
+import Foundation
+
+public struct ClientSettingDTO: Codable, Hashable, Sendable,
+								Identifiable {
+	public let id:                       UUID
+	public var enableSoundEffects:       Bool?
+	public var rememberSignIn:           Bool?
+	public var newPostNotifications:     Bool?
+	public var newStageNotifications:    Bool?
+	public var newStoryNotifications:    Bool?
+	public var likePostNotifications:    Bool?
+	public var likeStageNotifications:   Bool?
+	public var likeStoryNotifications:   Bool?
+	public var giftedCoinsNotifications: Bool?
+	public let created:                  Date?
+	public let updated:                  Date?
+
+	public init(
+		id:                       UUID  = UUID(),
+		enableSoundEffects:       Bool? = nil,
+		rememberSignIn:           Bool? = nil,
+		newPostNotifications:     Bool? = nil,
+		newStageNotifications:    Bool? = nil,
+		newStoryNotifications:    Bool? = nil,
+		likePostNotifications:    Bool? = nil,
+		likeStageNotifications:   Bool? = nil,
+		likeStoryNotifications:   Bool? = nil,
+		giftedCoinsNotifications: Bool? = nil,
+		created:                  Date? = nil,
+		updated:                  Date? = nil
+	) {
+		self.id                       = id
+		self.enableSoundEffects       = enableSoundEffects
+		self.rememberSignIn           = rememberSignIn
+		self.newPostNotifications     = newPostNotifications
+		self.newStageNotifications    = newStageNotifications
+		self.newStoryNotifications    = newStoryNotifications
+		self.likePostNotifications    = likePostNotifications
+		self.likeStageNotifications   = likeStageNotifications
+		self.likeStoryNotifications   = likeStoryNotifications
+		self.giftedCoinsNotifications = giftedCoinsNotifications
+		self.created                  = created
+		self.updated                  = updated
+	}
+}
