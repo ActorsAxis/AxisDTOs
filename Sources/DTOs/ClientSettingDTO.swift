7d03b57e5d7f4698d83758217c3137bb6619451b
William J. C. Nesbitt <wjcn@mac.com>
2025-02-24 23:16:59 -0800

    Renames giftedCoinsNotifications to giftCoinsNotifications in ClientSettingDTO.

--- Sources/DTOs/ClientSettingDTO.swift
+++ Sources/DTOs/ClientSettingDTO.swift
@@ -9,47 +9,47 @@ import Foundation
 
 public struct ClientSettingDTO: Codable, Hashable, Sendable,
 								Identifiable {
-	public let id:                       UUID
-	public var userID:                   UUID?
-	public var enableSoundEffects:       Bool?
-	public var rememberSignIn:           Bool?
-	public var newPostNotifications:     Bool?
-	public var newStageNotifications:    Bool?
-	public var newStoryNotifications:    Bool?
-	public var likePostNotifications:    Bool?
-	public var likeStageNotifications:   Bool?
-	public var likeStoryNotifications:   Bool?
-	public var giftedCoinsNotifications: Bool?
-	public let created:                  Date?
-	public let updated:                  Date?
+	public let id:                     UUID
+	public var userID:                 UUID?
+	public var enableSoundEffects:     Bool?
+	public var rememberSignIn:         Bool?
+	public var newPostNotifications:   Bool?
+	public var newStageNotifications:  Bool?
+	public var newStoryNotifications:  Bool?
+	public var likePostNotifications:  Bool?
+	public var likeStageNotifications: Bool?
+	public var likeStoryNotifications: Bool?
+	public var giftCoinsNotifications: Bool?
+	public let created:                Date?
+	public let updated:                Date?
 
 	public init(
-		id:                       UUID  = UUID(),
-		userID:                   UUID? = nil,
-		enableSoundEffects:       Bool? = nil,
-		rememberSignIn:           Bool? = nil,
-		newPostNotifications:     Bool? = nil,
-		newStageNotifications:    Bool? = nil,
-		newStoryNotifications:    Bool? = nil,
-		likePostNotifications:    Bool? = nil,
-		likeStageNotifications:   Bool? = nil,
-		likeStoryNotifications:   Bool? = nil,
-		giftedCoinsNotifications: Bool? = nil,
-		created:                  Date? = nil,
-		updated:                  Date? = nil
+		id:                     UUID  = UUID(),
+		userID:                 UUID? = nil,
+		enableSoundEffects:     Bool? = nil,
+		rememberSignIn:         Bool? = nil,
+		newPostNotifications:   Bool? = nil,
+		newStageNotifications:  Bool? = nil,
+		newStoryNotifications:  Bool? = nil,
+		likePostNotifications:  Bool? = nil,
+		likeStageNotifications: Bool? = nil,
+		likeStoryNotifications: Bool? = nil,
+		giftCoinsNotifications: Bool? = nil,
+		created:                Date? = nil,
+		updated:                Date? = nil
 	) {
-		self.id                       = id
-		self.userID                   = userID
-		self.enableSoundEffects       = enableSoundEffects
-		self.rememberSignIn           = rememberSignIn
-		self.newPostNotifications     = newPostNotifications
-		self.newStageNotifications    = newStageNotifications
-		self.newStoryNotifications    = newStoryNotifications
-		self.likePostNotifications    = likePostNotifications
-		self.likeStageNotifications   = likeStageNotifications
-		self.likeStoryNotifications   = likeStoryNotifications
-		self.giftedCoinsNotifications = giftedCoinsNotifications
-		self.created                  = created
-		self.updated                  = updated
+		self.id                     = id
+		self.userID                 = userID
+		self.enableSoundEffects     = enableSoundEffects
+		self.rememberSignIn         = rememberSignIn
+		self.newPostNotifications   = newPostNotifications
+		self.newStageNotifications  = newStageNotifications
+		self.newStoryNotifications  = newStoryNotifications
+		self.likePostNotifications  = likePostNotifications
+		self.likeStageNotifications = likeStageNotifications
+		self.likeStoryNotifications = likeStoryNotifications
+		self.giftCoinsNotifications = giftCoinsNotifications
+		self.created                = created
+		self.updated                = updated
 	}
 }
