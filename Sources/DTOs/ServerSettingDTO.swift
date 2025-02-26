1a81a8992bfdf5cd2f9a686107af8148bb2e315c
William J. C. Nesbitt <wjcn@mac.com>
2025-02-25 19:43:06 -0800

    Adds missions to ServerSettingDTO.

--- Sources/DTOs/ServerSettingDTO.swift
+++ Sources/DTOs/ServerSettingDTO.swift
@@ -9,17 +9,41 @@ import Foundation
 
 public struct ServerSettingDTO: Codable, Hashable, Sendable,
 								Identifiable {
-	public let id:      UUID
-	public let created: Data?
-	public let updated: Data?
+	public let id:       UUID
+	public var missions: Missions?
+	public let created:  Data?
+	public let updated:  Data?
 
 	public init(
-		id:      UUID  = UUID(),
-		created: Data? = nil,
-		updated: Data? = nil
+		id:       UUID      = UUID(),
+		missions: Missions? = nil,
+		created:  Data?     = nil,
+		updated:  Data?     = nil
 	) {
-		self.id      = id
-		self.created = created
-		self.updated = updated
+		self.id       = id
+		self.missions = missions
+		self.created  = created
+		self.updated  = updated
+	}
+}
+
+// MARK: -
+
+public struct Missions: Codable, Hashable, Sendable {
+	public var coinGiveawayRequirement: Int?
+	public var liveStreamRequirement:   Int?
+	public var postRequirement:         Int?
+	public var stageRequirement:        Int?
+
+	public init(
+		coinGiveawayRequirement: Int? = nil,
+		liveStreamRequirement:   Int? = nil,
+		postRequirement:         Int? = nil,
+		stageRequirement:        Int? = nil
+	) {
+		self.coinGiveawayRequirement = coinGiveawayRequirement
+		self.liveStreamRequirement   = liveStreamRequirement
+		self.postRequirement         = postRequirement
+		self.stageRequirement        = stageRequirement
 	}
 }
