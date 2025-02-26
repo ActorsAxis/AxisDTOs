c56d414ffb1203668334eea407570406063fb7e4
William J. C. Nesbitt <wjcn@mac.com>
2025-02-26 13:01:44 -0800

    Adds 9 fields to ProductDTO.

--- Sources/DTOs/ProductDTO.swift
+++ Sources/DTOs/ProductDTO.swift
@@ -9,17 +9,44 @@ import Foundation
 
 public struct ProductDTO: Codable, Hashable, Sendable,
 						  Identifiable {
-	public let id:      UUID
-	public let created: Date?
-	public let updated: Date?
+	public let id:               UUID
+	public var stockKeepingUnit: String?
+	public var title:            String?
+	public var shortDescription: String?
+	public var longDescription:  String?
+	public var url:              String?
+	public var order:            Int?
+	public var level:            Int?
+	public var quantity:         Int?
+	public var discount:         Double?
+	public let created:          Date?
+	public let updated:          Date?
 
 	public init(
-		id:      UUID  = UUID(),
-		created: Date? = nil,
-		updated: Date? = nil
+		id:               UUID    = UUID(),
+		stockKeepingUnit: String? = nil,
+		title:            String? = nil,
+		shortDescription: String? = nil,
+		longDescription:  String? = nil,
+		url:              String? = nil,
+		order:            Int?    = nil,
+		level:            Int?    = nil,
+		quantity:         Int?    = nil,
+		discount:         Double? = nil,
+		created:          Date?   = nil,
+		updated:          Date?   = nil
 	) {
-		self.id      = id
-		self.created = created
-		self.updated = updated
+		self.id               = id
+		self.stockKeepingUnit = stockKeepingUnit
+		self.title            = title
+		self.shortDescription = shortDescription
+		self.longDescription  = longDescription
+		self.url              = url
+		self.order            = order
+		self.level            = level
+		self.quantity         = quantity
+		self.discount         = discount
+		self.created          = created
+		self.updated          = updated
 	}
 }
