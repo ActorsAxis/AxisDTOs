d327da288fb4176ac9d3a9880b894d06954c611f
William J. C. Nesbitt <wjcn@mac.com>
2025-05-04 15:27:29 -0700

    Renames description (back) to postDescription (again). Adds comment as to why this is/was a good idea.

--- Sources/DTOs/PostDTO.swift
+++ Sources/DTOs/PostDTO.swift
@@ -9,76 +9,56 @@ import Foundation
 
 public struct PostDTO: Codable,  Sendable,
 					   Hashable, Identifiable {
-	public let id:          UUID
-	public var postTypeID:  UUID?
-	public var postType:    PostTypeDTO?
-	public var userID:      UUID?
-	public var user:        UserDTO?
-	public var coordinates: Coordinates?
-	public var name:        String?
-	public var description: String?
-	public var url:         String?
-	public var media:      [MediumDTO]?
-	public var comments:    Int?
-	public var favorites:   Int
-	public var likes:       Int
-	public var created:     Date?
-	public var updated:     Date?
-
-	public var postDescription: String? {
-		get { description            }
-		set { description = newValue }
-	}
+	public let id:              UUID
+	public var postTypeID:      UUID?
+	public var postType:        PostTypeDTO?
+	public var userID:          UUID?
+	public var user:            UserDTO?
+	public var coordinates:     Coordinates?
+	public var name:            String?
+	// The “post” prefix before “description” is redundant on
+	// “PostDTO,” but “description” alone would conflict with
+	// future CustomStringConvertible protocol conformance.
+	public var postDescription: String?
+	public var url:             String?
+	public var media:          [MediumDTO]?
+	public var comments:        Int?
+	public var favorites:       Int
+	public var likes:           Int
+	public var created:         Date?
+	public var updated:         Date?
 
 	public init(
-		id:          UUID         = UUID(),
-		postTypeID:  UUID?        = nil,
-		postType:    PostTypeDTO? = nil,
-		userID:      UUID?        = nil,
-		user:        UserDTO?     = nil,
-		coordinates: Coordinates? = nil,
-		name:        String?      = nil,
-		description: String?      = nil,
-		url:         String?      = nil,
-		media:      [MediumDTO]?  = nil,
-		comments:    Int?         = nil,
-		favorites:   Int          = 0,
-		likes:       Int          = 0,
-		created:     Date?        = nil,
-		updated:     Date?        = nil
+		id:              UUID         = UUID(),
+		postTypeID:      UUID?        = nil,
+		postType:        PostTypeDTO? = nil,
+		userID:          UUID?        = nil,
+		user:            UserDTO?     = nil,
+		coordinates:     Coordinates? = nil,
+		name:            String?      = nil,
+		postDescription: String?      = nil,
+		url:             String?      = nil,
+		media:          [MediumDTO]?  = nil,
+		comments:        Int?         = nil,
+		favorites:       Int          = 0,
+		likes:           Int          = 0,
+		created:         Date?        = nil,
+		updated:         Date?        = nil
 	) {
-		self.id          = id
-		self.postTypeID  = postTypeID
-		self.postType    = postType
-		self.userID      = userID
-		self.user        = user
-		self.coordinates = coordinates
-		self.name        = name
-		self.description = description
-		self.url         = url
-		self.media       = media
-		self.comments    = comments
-		self.favorites   = favorites
-		self.likes       = likes
-		self.created     = created
-		self.updated     = updated
-	}
-
-	enum CodingKeys: String, CodingKey {
-		case id
-		case postTypeID
-		case postType
-		case userID
-		case user
-		case coordinates
-		case name
-		case description = "postDescription" // For Backward Compatibility
-		case url
-		case media
-		case comments
-		case favorites
-		case likes
-		case created
-		case updated
+		self.id              = id
+		self.postTypeID      = postTypeID
+		self.postType        = postType
+		self.userID          = userID
+		self.user            = user
+		self.coordinates     = coordinates
+		self.name            = name
+		self.postDescription = postDescription
+		self.url             = url
+		self.media           = media
+		self.comments        = comments
+		self.favorites       = favorites
+		self.likes           = likes
+		self.created         = created
+		self.updated         = updated
 	}
 }
