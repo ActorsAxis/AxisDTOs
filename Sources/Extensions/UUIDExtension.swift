54d4ad5e339d86874f8b0ad1d8706d2fb255fdbd
William J. C. Nesbitt <wjcn@mac.com>
2025-06-06 10:30:46 -0700

    Fixes UUID.uniqueID again.

--- Sources/Extensions/UUIDExtension.swift
+++ Sources/Extensions/UUIDExtension.swift
@@ -1,3 +1,5 @@
+#warning("This extension should be moved to the iOS app project.")
+
 //
 //  UUIDExtension.swift
 //  AxisDTOs
@@ -8,31 +10,34 @@
 import Foundation
 
 extension UUID {
-	public var uniqueID: UInt { low ^ high }
-
-	public var low: UInt {
-		var low: UInt = 0
-		low |= UInt(uuid.0) << (uuid.0.bitWidth * 0)
-		low |= UInt(uuid.1) << (uuid.1.bitWidth * 1)
-		low |= UInt(uuid.2) << (uuid.2.bitWidth * 2)
-		low |= UInt(uuid.3) << (uuid.3.bitWidth * 3)
-		low |= UInt(uuid.4) << (uuid.4.bitWidth * 4)
-		low |= UInt(uuid.5) << (uuid.5.bitWidth * 5)
-		low |= UInt(uuid.6) << (uuid.6.bitWidth * 6)
-		low |= UInt(uuid.7) << (uuid.7.bitWidth * 7)
-		return low
+	public var uniqueID: UInt32 {
+		let tuple = uInt32
+		return tuple.0 ^ tuple.1 ^ tuple.2 ^ tuple.3
 	}
 
-	public var high: UInt {
-		var high: UInt = 0
-		high |= UInt(uuid .8) << (uuid .8.bitWidth * 0)
-		high |= UInt(uuid .9) << (uuid .9.bitWidth * 1)
-		high |= UInt(uuid.10) << (uuid.10.bitWidth * 2)
-		high |= UInt(uuid.11) << (uuid.11.bitWidth * 3)
-		high |= UInt(uuid.12) << (uuid.12.bitWidth * 4)
-		high |= UInt(uuid.13) << (uuid.13.bitWidth * 5)
-		high |= UInt(uuid.14) << (uuid.14.bitWidth * 6)
-		high |= UInt(uuid.15) << (uuid.15.bitWidth * 7)
-		return high
+	private var uInt32: (UInt32, UInt32, UInt32, UInt32) {
+		var tuple: (UInt32, UInt32, UInt32, UInt32) = (0, 0, 0, 0)
+
+		tuple.0 |= UInt32(uuid .0) << (uuid .0.bitWidth * 0)
+		tuple.0 |= UInt32(uuid .1) << (uuid .1.bitWidth * 1)
+		tuple.0 |= UInt32(uuid .2) << (uuid .2.bitWidth * 2)
+		tuple.0 |= UInt32(uuid .3) << (uuid .3.bitWidth * 3)
+
+		tuple.1 |= UInt32(uuid .4) << (uuid .4.bitWidth * 0)
+		tuple.1 |= UInt32(uuid .5) << (uuid .5.bitWidth * 1)
+		tuple.1 |= UInt32(uuid .6) << (uuid .6.bitWidth * 2)
+		tuple.1 |= UInt32(uuid .7) << (uuid .7.bitWidth * 3)
+
+		tuple.2 |= UInt32(uuid .8) << (uuid .8.bitWidth * 0)
+		tuple.2 |= UInt32(uuid .9) << (uuid .9.bitWidth * 1)
+		tuple.2 |= UInt32(uuid.10) << (uuid.10.bitWidth * 2)
+		tuple.2 |= UInt32(uuid.11) << (uuid.11.bitWidth * 3)
+
+		tuple.3 |= UInt32(uuid.12) << (uuid.12.bitWidth * 0)
+		tuple.3 |= UInt32(uuid.13) << (uuid.13.bitWidth * 1)
+		tuple.3 |= UInt32(uuid.14) << (uuid.14.bitWidth * 2)
+		tuple.3 |= UInt32(uuid.15) << (uuid.15.bitWidth * 3)
+
+		return tuple
 	}
 }
